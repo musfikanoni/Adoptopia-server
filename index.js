621ef574a5686ebcbd835722d04f52a2f16b4548
musfikanoni <musfikanoni@gmail.com>
2025-01-20 21:00:23 +0600

    donation campaigns details api

--- index.js
+++ index.js
@@ -49,6 +49,14 @@ async function run() {
       res.send(result);
   })
 
+  //donation campaigns details
+  app.get('/donationCampaign/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await donationCampaignCollection.findOne(query);
+    res.send(result);
+  })
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
